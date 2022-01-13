3d8ea5da9901fa62fb4c9d1e0cdd7592f7c7754b
Harsh Kalyani <65092789+hkaly@users.noreply.github.com>
2022-01-13 15:39:36 -0500

    Updated function description and prints error statement if system has not installed docx2txt library

--- Development/extract.py
+++ Development/extract.py
@@ -1,12 +1,19 @@
-import docx2txt
+import sys
 
-def extractTextFromDocx():
+try:
+    import docx2txt
+except:
+    print('Error:\tdocx2txt library has not been installed.\n\tTry \'pip install docx2txt\'.')
+    sys.exit(0)
+
+def extractTextFromDocx(fileName, imagePath):
     '''
+    A function that extracts all images and text (excluding math equations) from docx files.
+
     Requirements: pip install docx2txt
-    This function extracts text (excluding math equations) and images from docx files.
-    Text is stored in variable. Images are stored in file path (second parameter of process() function). 
-    '''
-    text = docx2txt.process("Team-150-Assessment12T.docx", "img") #second parameter is file path where images 
-    print(text)
 
-extractTextFromDocx()
+    Args: Must be string type variables
+
+    Text is stored in variable. Images are stored in file path.
+    '''
+    text = docx2txt.process(fileName, imagePath) #first parameter is file name and second is file path for extracted images
