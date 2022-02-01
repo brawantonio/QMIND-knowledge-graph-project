6993f53ec108b814933408a858e27200f19cb8f4
Braulio Antonio <braw.a.chi@gmail.com>
2022-01-31 19:36:28 -0500

    Revert "Made changes to pdf reader so that it now returns a list of sentences…"

--- Development/pdfr_usage.py
+++ Development/pdfr_usage.py
@@ -7,11 +7,13 @@ This script illustrates how to implement the functions from 'PDFReader' into a
 """
 
 # Method 1
-from PDFReader import readPDF
+from PDFReader import readPDF, printPDF
 
-pdf1 = readPDF('test_files/Test1PDF.pdf')
+pdf1 = readPDF('test_files/Test2PDF.pdf')
+printPDF(pdf1)
 
 # Method 2 
 import PDFReader
 
 pdf2 = PDFReader.readPDF('test_files/Test3PDF.pdf')
+PDFReader.printPDF(pdf2)
